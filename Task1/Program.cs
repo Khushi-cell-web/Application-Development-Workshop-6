b8db4b1070baaf65b3bd804e53c917d425df315d
Khushi-cell-web <khushibohora692@gmail.com>
2025-12-04 10:51:55 +0545

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Drawing;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var rect = new Rectangle { Length = 10, Width = 5 };
+            Console.WriteLine($"Area = {rect.Area}");
+            Console.WriteLine($"Perimeter = {rect.Perimeter}");
+
+        }
+    }
+}
