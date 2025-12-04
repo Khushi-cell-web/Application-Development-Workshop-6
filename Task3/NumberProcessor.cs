5127e0fd053f1cf04fc9e9bee2361a1a28fe4e57
Khushi-cell-web <khushibohora692@gmail.com>
2025-12-04 11:03:35 +0545

    Task3

---
+++ Task3/NumberProcessor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    public class NumberProcessor
+    {
+        public void ProcessNumbers(int[] numbers, Func<int, bool> condition)
+        {
+            foreach (var n in numbers)
+            {
+                if (condition(n))
+                    Console.WriteLine(n);
+            }
+        }
+    }
+
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var np = new NumberProcessor();
+            int[] nums = { 2, 4, 7, 12, 18, 3 };
+
+            np.ProcessNumbers(nums, n => n % 2 == 0);       // Even numbers
+            np.ProcessNumbers(nums, n => n > 10);          // > 10
+
+        }
+    }
+}
