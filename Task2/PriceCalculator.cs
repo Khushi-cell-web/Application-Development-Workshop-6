32c5ea356728068bfe2861ddf7541ff092c32e04
Khushi-cell-web <khushibohora692@gmail.com>
2025-12-04 10:59:46 +0545

    Task2

---
+++ Task2/PriceCalculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public class PriceCalculator
+    {
+        public double CalculateFinalPrice(double originalPrice, DiscountStrategy strategy)
+        {
+            return strategy(originalPrice);
+        }
+    }
+
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿using Task2;
+
+var service = new DiscountService();
+var calc = new PriceCalculator();
+
+// Delegate for Add & Subtract
+Calculate c1 = service.Add;
+Calculate c2 = service.Subtract;
+
+Console.WriteLine("Add: " + c1(10, 5));
+Console.WriteLine("Subtract: " + c2(10, 5));
+
+// Discount Strategy delegate
+Console.WriteLine(calc.CalculateFinalPrice(1000, service.FestivalDiscount));
+Console.WriteLine(calc.CalculateFinalPrice(1000, service.SeasonalDiscount));
+Console.WriteLine(calc.CalculateFinalPrice(1000, service.NoDiscount));
+
+// Lambda discount (30% off)
+Console.WriteLine(calc.CalculateFinalPrice(1000, p => p * 0.70));
